ed0414f8a053c5d5fae4e2133e9d8d2462ddf119
Bob Polacheck <rpolacheck@gmail.com>
2018-09-24 22:09:46 +0000

    Added Schema and hooked up mongodb

--- app.js
+++ app.js
@@ -3,3 +3,30 @@ mongoose    = require("mongoose"),
 bodyParser  = require("body-parser"),
 app         = express();
 
+var url = "mongodb://localhost/restful_blog_app";
+mongoose.connect(url, { useNewUrlParser: true }, function(err, db){
+  if(err){
+      console.log(err);
+  }  else {
+      console.log('connected to ' + url);
+      db.close();
+  }
+});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+var blogSchema = new mongoose.Schema({
+   title: String,
+   image: String,
+   body: String,
+   created: {type: Date, default: Date.now}
+});
+// title
+// image
+// body
+// created
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is Running");
+});
