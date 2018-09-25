e73781ec454d8935bbc61ef042eec5283466bf08
Bob Polacheck <rpolacheck@gmail.com>
2018-09-25 03:54:20 +0000

    Create route was added along with styling the /new page

--- app.js
+++ app.js
@@ -42,6 +42,18 @@ app.get("/blogs/new", function(req, res){
 });
 
 // Create Route
+app.post("/blogs", function(req, res){
+   // create blog
+   Blog.create(req.body.blog, function(err, newBlog){
+       if(err){
+            res.render("new");
+       } else {
+            // redirect to index
+            res.redirect("/blogs");
+       }
+   });
+   
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is Running");
