59abbcda541bbeca580685c9844ebe99ce9a3123
Bob Polacheck <rpolacheck@gmail.com>
2018-09-26 21:27:42 +0000

    Rendered show page and route

--- app.js
+++ app.js
@@ -55,6 +55,18 @@ app.post("/blogs", function(req, res){
    
 });
 
+// Show Route
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.render("show", {blog: foundBlog});
+       }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is Running");
 });
